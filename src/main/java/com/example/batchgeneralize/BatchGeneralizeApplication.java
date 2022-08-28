93fc46fb646b09477e244bc5b1890c87c098616a
ksuraweera <ksuraweera@mitrai.com>
2022-08-28 10:31:27 +0530

    initial project setup

---
+++ src/main/java/com/example/batchgeneralize/BatchGeneralizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchgeneralize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchGeneralizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchGeneralizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/batchgeneralize/BatchGeneralizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchgeneralize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchGeneralizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
